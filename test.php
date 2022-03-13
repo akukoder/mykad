602e3b8eb169a2275ba991d189eb8aa047ed193c
Syahril Zulkefli <syahzul@gmail.com>
2022-03-13 22:08:22 +0800

    Remove from commit

--- test.php
+++ test.php
@@ -2,10 +2,14 @@
 require './vendor/autoload.php';
 
 use AkuKoder\MyKad\Extractor;
+use AkuKoder\MyKad\Faker\MyKadProvider;
 use AkuKoder\MyKad\Internal\Gender;
+use AkuKoder\MyKad\Validator;
 
 
 //print (new Extractor('871003417888'))->dateOfBirth('d/m/Y');
 //print (new Extractor('871003417888'))->gender();
 //print (new Extractor('871003417888'))->stateName();
-print (new Gender('871003417887'))->value;
+$mykad = (new MyKadProvider)->mykad();
+echo $mykad."\n";
+dd((new Validator())->verifyStateCode($mykad));
