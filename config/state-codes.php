b24cc83c03af992d2f4512f5fddf0194c06702c1
Syahril Zulkefli <syahzul@gmail.com>
2022-03-13 22:08:51 +0800

    Use config file for state codes

---
+++ config/state-codes.php
@@ -0,0 +1,95 @@
+<?php
+/*
+ * Extracted from https://www.jpn.gov.my/my/kod-negeri, on 2022-03-13
+ */
+return [
+    // Johor
+    '01' => 'Johor',
+    '21' => 'Johor',
+    '22' => 'Johor',
+    '23' => 'Johor',
+    '24' => 'Johor',
+
+    // Kedah
+    '02' => 'Kedah',
+    '25' => 'Kedah',
+    '26' => 'Kedah',
+    '27' => 'Kedah',
+
+    // Kelantan
+    '03' => 'Kelantan',
+    '28' => 'Kelantan',
+    '29' => 'Kelantan',
+
+    // Melaka
+    '04' => 'Melaka',
+    '30' => 'Melaka',
+
+    // Negeri Sembilan
+    '05' => 'Negeri Sembilan',
+    '31' => 'Negeri Sembilan',
+    '59' => 'Negeri Sembilan',
+
+    // Pahang
+    '06' => 'Pahang',
+    '32' => 'Pahang',
+    '33' => 'Pahang',
+
+    // Pulau Pinang
+    '07' => 'Pulau Pinang',
+    '34' => 'Pulau Pinang',
+    '35' => 'Pulau Pinang',
+
+    // Perak
+    '08' => 'Perak',
+    '36' => 'Perak',
+    '37' => 'Perak',
+    '38' => 'Perak',
+    '39' => 'Perak',
+
+    // Perlis
+    '09' => 'Perlis',
+    '40' => 'Perlis',
+
+    // Selangor
+    '10' => 'Selangor',
+    '41' => 'Selangor',
+    '42' => 'Selangor',
+    '43' => 'Selangor',
+    '44' => 'Selangor',
+
+    // Terengganu
+    '11' => 'Terengganu',
+    '45' => 'Terengganu',
+    '46' => 'Terengganu',
+
+    // Sabah
+    '12' => 'Sabah',
+    '47' => 'Sabah',
+    '48' => 'Sabah',
+    '49' => 'Sabah',
+
+    // Sarawak
+    '13' => 'Sarawak',
+    '50' => 'Sarawak',
+    '51' => 'Sarawak',
+    '52' => 'Sarawak',
+    '53' => 'Sarawak',
+
+    // Wilayah Persekutuan (Kuala Lumpur)
+    '14' => 'Wilayah Persekutuan Kuala Lumpur',
+    '54' => 'Wilayah Persekutuan Kuala Lumpur',
+    '55' => 'Wilayah Persekutuan Kuala Lumpur',
+    '56' => 'Wilayah Persekutuan Kuala Lumpur',
+    '57' => 'Wilayah Persekutuan Kuala Lumpur',
+
+    // Wilayah Persekutuan (Labuan)
+    '15' => 'Wilayah Persekutuan Labuan',
+    '58' => 'Wilayah Persekutuan Labuan',
+
+    // Wilayah Persekutuan (Putrajaya)
+    '16' => 'Wilayah Persekutuan Putrajaya',
+
+    // Negeri Tidak Diketahui
+    '82' => 'Unknown',
+];
--- src/Internal/State.php
+++ src/Internal/State.php
@@ -2,15 +2,18 @@
 
 namespace AkuKoder\MyKad\Internal;
 
+use AkuKoder\MyKad\Exceptions\InvalidCodeException;
+
 class State
 {
-    const INVALID_VALUE = 'Invalid';
+    protected array $stateCodes = [];
 
     public string $name;
 
     public function __construct($input)
     {
         $this->name = $this->name($input);
+        $this->stateCodes = require __DIR__.'/../config/state-codes.php';
     }
 
     /**
@@ -24,116 +27,10 @@ class State
     {
         $code = substr($input, 6, 2);
 
-        switch ($code) {
-            // Johor
-            case '01':
-            case '21':
-            case '22':
-            case '23':
-            case '24':
-                return 'Johor';
-
-            // Kedah
-            case '02':
-            case '25':
-            case '26':
-            case '27':
-                return 'Kedah';
-
-            // Kelantan
-            case '03':
-            case '28':
-            case '29':
-                return 'Kelantan';
-
-            // Melaka
-            case '04':
-            case '30':
-                return 'Melaka';
-
-            // Negeri Sembilan
-            case '05':
-            case '31':
-            case '59':
-                return 'Negeri Sembilan';
-
-            // Pahang
-            case '06':
-            case '32':
-            case '33':
-                return 'Pahang';
-
-            // Pulau Pinang
-            case '07':
-            case '34':
-            case '35':
-                return 'Pulau Pinang';
-
-            // Perak
-            case '08':
-            case '36':
-            case '37':
-            case '38':
-            case '39':
-                return 'Perak';
-
-            // Perlis
-            case '09':
-            case '40':
-                return 'Perlis';
-
-            // Selangor
-            case '10':
-            case '41':
-            case '42':
-            case '43':
-            case '44':
-                return 'Selangor';
-
-            // Terengganu
-            case '11':
-            case '45':
-            case '46':
-                return 'Terengganu';
-
-            // Sabah
-            case '12':
-            case '47':
-            case '48':
-            case '49':
-                return 'Sabah';
-
-            // Sarawak
-            case '13':
-            case '50':
-            case '51':
-            case '52':
-            case '53':
-                return 'Sarawak';
-
-            // Wilayah Persekutuan (Kuala Lumpur)
-            case '14':
-            case '54':
-            case '55':
-            case '56':
-            case '57':
-                return 'Wilayah Persekutuan Kuala Lumpur';
-
-            // Wilayah Persekutuan (Labuan)
-            case '15':
-            case '58':
-                return 'Wilayah Persekutuan Labuan';
-
-            // Wilayah Persekutuan (Putrajaya)
-            case '16':
-                return 'Wilayah Persekutuan Putrajaya';
-
-            // Negeri Tidak Diketahui
-            case '82':
-                return 'Unknown';
-
-            default:
-                return self::INVALID_VALUE;
+        if (! array_key_exists($code, $this->stateCodes)) {
+            throw new InvalidCodeException;
         }
+
+        return $this->stateCodes[$code];
     }
 }
