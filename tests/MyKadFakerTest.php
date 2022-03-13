2e2250118572e8d8e3cea3dd5f152c5b1b8701ac
Syahril Zulkefli <syahzul@gmail.com>
2022-03-13 23:03:40 +0800

    Fix test

--- tests/MyKadFakerTest.php
+++ tests/MyKadFakerTest.php
@@ -6,17 +6,29 @@ use AkuKoder\MyKad\Cleaner;
 use AkuKoder\MyKad\Faker\MyKadProvider;
 use AkuKoder\MyKad\Validator;
 use Orchestra\Testbench\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
 
 class MyKadFakerTest extends TestCase
 {
+    use WithFaker;
+
     protected function setUp(): void
     {
         parent::setUp();
     }
 
+    public function testGeneratedString()
+    {
+        $this->faker->addProvider(new MyKadProvider($this->faker));
+
+        $this->assertMatchesRegularExpression('/\w+/', $this->faker->mykad);
+    }
+
     public function testGeneratedStringIsValid()
     {
-        $mykad = (new MyKadProvider)->mykad();
+        $this->faker->addProvider(new MyKadProvider($this->faker));
+
+        $mykad = $this->faker->mykad;
 
         $validated = (new Validator())->validate($mykad);
 
